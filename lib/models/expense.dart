01ef8491a83df6ed3eef6ce968f350fda5292271
Mantas <mantasx5@gmail.com>
2023-09-24 21:03:59 +0200

    Added enum allowed categories of expense

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -2,12 +2,19 @@ import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
 
+enum Category { food, transportation, leisure, work }
+
 class Expense {
   final String id;
   final String title;
   final double amount;
   final DateTime date;
+  final Category category;
 
-  Expense({required this.title, required this.amount, required this.date})
-      : id = uuid.v4();
+  Expense({
+    required this.title,
+    required this.amount,
+    required this.date,
+    required this.category,
+  }) : id = uuid.v4();
 }
