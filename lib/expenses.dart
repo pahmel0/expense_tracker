75718ce53600b3e5340fc5076a82d32922caceaa
Mantas <mantasx5@gmail.com>
2023-09-24 21:50:05 +0200

    Added expenses list in UI

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/expenses_list.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({Key? key}) : super(key: key);
@@ -24,9 +25,12 @@ class _ExpensesState extends State<Expenses> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
-        children: [Text('The chart'), Text('Expenses list')],
+        children: [
+          const Text('The chart'),
+          Expanded(child: ExpensesList(expenses: _registeredExpenses)),
+        ],
       ),
     );
   }
