3234d7c59a3806ed51ab04cae006c3ffa3e4137a
Mantas <mantasx5@gmail.com>
2023-10-02 11:18:43 +0200

    Added ExpenseBucket class which represents logic behind showing visual representation of expense graph

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -31,3 +31,17 @@ class Expense {
 
   String get formattedDate => formatter.format(date);
 }
+
+class ExpenseBucket {
+  final Category category;
+  final List<Expense> expenses;
+
+  ExpenseBucket({required this.category, required this.expenses});
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses
+            .where((expense) => expense.category == category)
+            .toList();
+
+  double get totalExpenses =>
+      expenses.fold(0, (sum, expense) => sum + expense.amount);
+}
